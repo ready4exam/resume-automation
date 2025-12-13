f7e8458848a228a841dab3435be88ba89367f618
ready4exam <ready4urexam@gmail.com>
2025-12-13 16:21:13 +0530

    Update refine_resume.mjs

--- refine_resume.mjs
+++ refine_resume.mjs
@@ -4,7 +4,8 @@
 
 import fs from "fs";
 import path from "path";
-import { GoogleGenerativeAI } from "@google-generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 import {
   Document,
   Packer,
